67f182e8f734b988c2e8545974aad5bfe8808b2e
Jesse Kriss <jesse@jklabs.net>
2010-11-19 15:01:30 -0800

    writing out to public/javascripts/coffeescripts.js on every development request

--- app/controllers/bistro_car/bundle_controller.rb
+++ app/controllers/bistro_car/bundle_controller.rb
@@ -4,5 +4,13 @@ class BistroCar::BundleController < ActionController::Base
   def show
     headers['Content-Type'] = "application/javascript"
     render :text => BistroCar::Bundle.new(params[:bundle]).to_javascript
+    
+    # if we're in developement, render out the javascript file, too
+    if Rails.env.development?
+      path = "public/javascripts/coffeescripts.js"
+      BistroCar.minify = true
+      File.open(path, "w") { |file| file << BistroCar::Bundle.new('default').to_javascript }
+    end
+
   end
 end
